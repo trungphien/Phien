0d08c8180bb18e581d25dc8952966354a08c43a2
Phien <trungphien456@gmail.com>
2024-04-13 10:05:02 +0700

    all

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
 import StopWatch from './Watch'
-
 
 AppRegistry.registerComponent(appName, () => StopWatch);
